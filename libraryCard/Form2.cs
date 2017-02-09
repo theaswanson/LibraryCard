ddf737ef206e7a5108a3177f13f602625fa57a0e
Kobe Raypole <KDRaypole01@indianatech.net>
2017-02-09 14:28:27 -0500

    Added the ability to add books into the database

--- libraryCard/Form2.cs
+++ libraryCard/Form2.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
 
 namespace libraryCard
 {
@@ -40,12 +42,31 @@ namespace libraryCard
         private void getData_Click(object sender, EventArgs e)
         {
             //Console.WriteLine(bookTitle);
-            MessageBox.Show("Title: " + this.bookTitle.Text +
+            /*MessageBox.Show("Title: " + this.bookTitle.Text +
                 "\nAuthor: " + this.author.Text +
                 "\nISBN: " + this.ISBN.Text +
                 "\nGenre: " + this.genre.Text +
                 "\nYear Published: " + this.year.Text +
-                "\nPage Count: " + this.pageCount.Text);
+                "\nPage Count: " + this.pageCount.Text);*/
+            string constring = "datasource=teh.ddns.net;port=3306;username=scrub;password=librarycard";
+            string Query = "INSERT INTO librarycard.books (title,author,ISBN,genre,pageCount) values('" + this.bookTitle.Text + "','" + this.author.Text + "','" + this.ISBN.Text + "','" + this.genre.Text + "','" + this.pageCount.Text + "') ;";
+            MySqlConnection conDataBase = new MySqlConnection(constring);
+            MySqlCommand cmdDataBase = new MySqlCommand(Query, conDataBase);
+            MySqlDataReader myReader;
+            try
+            {
+                conDataBase.Open();
+                myReader = cmdDataBase.ExecuteReader();
+                MessageBox.Show("Book Saved");
+                while (myReader.Read())
+                {
+
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+                
         }
 
         private void condFair_CheckedChanged(object sender, EventArgs e)
