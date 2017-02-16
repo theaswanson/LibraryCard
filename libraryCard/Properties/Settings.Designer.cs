d5bd021ece91e9aa3dd4fcc0144ba9e684e3095b
Adam Swanson <stapeowner@gmail.com>
2017-02-16 08:39:50 -0500

    Settings, App.config, librarycardDataSet

--- libraryCard/Properties/Settings.Designer.cs
+++ libraryCard/Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace libraryCard.Properties {
                 return ((string)(this["libraryCardDataConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("server=teh.ddns.net;user id=scrub;database=librarycard;allowuservariables=True;pa" +
+            "ssword=librarycard")]
+        public string librarycardConnectionString {
+            get {
+                return ((string)(this["librarycardConnectionString"]));
+            }
+        }
     }
 }
