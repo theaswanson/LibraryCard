0dd68ed5fa97fec1fbd60e13ee9f651d60f54541
Serghei Mineev <SMineev01@indianatech.net>
2017-02-21 09:39:57 -0500

    Just trust me.. commit

--- libraryCard/Form5.Designer.cs
+++ libraryCard/Form5.Designer.cs
@@ -31,11 +31,16 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form5));
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.lname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(689, 710);
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(302, 228);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(104, 40);
             this.button2.TabIndex = 9;
@@ -45,18 +50,60 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(545, 710);
+            this.button1.Location = new System.Drawing.Point(147, 228);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(104, 40);
+            this.button1.Size = new System.Drawing.Size(115, 40);
             this.button1.TabIndex = 8;
-            this.button1.Text = "Add";
+            this.button1.Text = "Submit";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lname
+            // 
+            this.lname.Location = new System.Drawing.Point(200, 147);
+            this.lname.Margin = new System.Windows.Forms.Padding(4);
+            this.lname.Name = "lname";
+            this.lname.Size = new System.Drawing.Size(285, 29);
+            this.lname.TabIndex = 19;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 147);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 25);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Book ID";
+            // 
+            // fname
+            // 
+            this.fname.Location = new System.Drawing.Point(198, 69);
+            this.fname.Margin = new System.Windows.Forms.Padding(4);
+            this.fname.Name = "fname";
+            this.fname.Size = new System.Drawing.Size(287, 29);
+            this.fname.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 69);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 25);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Customer ID";
             // 
             // Form5
             // 
+            this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1276, 836);
+            this.CancelButton = this.button2;
+            this.ClientSize = new System.Drawing.Size(568, 318);
+            this.Controls.Add(this.lname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fname);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -65,6 +112,7 @@
             this.Text = "Checking-In";
             this.TopMost = true;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,5 +120,9 @@
 
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox lname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox fname;
+        private System.Windows.Forms.Label label1;
     }
 }
--- libraryCard/Form6.Designer.cs
+++ libraryCard/Form6.Designer.cs
@@ -29,50 +29,100 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form6));
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.lname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button2
+            // lname
             // 
-            this.button2.Location = new System.Drawing.Point(655, 719);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(105, 41);
-            this.button2.TabIndex = 13;
-            this.button2.Text = "Cancel";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.lname.Location = new System.Drawing.Point(226, 161);
+            this.lname.Margin = new System.Windows.Forms.Padding(4);
+            this.lname.Name = "lname";
+            this.lname.Size = new System.Drawing.Size(285, 29);
+            this.lname.TabIndex = 25;
             // 
-            // button1
+            // label2
             // 
-            this.button1.Location = new System.Drawing.Point(514, 719);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(105, 41);
-            this.button1.TabIndex = 12;
-            this.button1.Text = "Add";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 161);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 25);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Book ID";
+            // 
+            // fname
+            // 
+            this.fname.Location = new System.Drawing.Point(224, 83);
+            this.fname.Margin = new System.Windows.Forms.Padding(4);
+            this.fname.Name = "fname";
+            this.fname.Size = new System.Drawing.Size(287, 29);
+            this.fname.TabIndex = 24;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 83);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 25);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Customer ID";
+            // 
+            // button3
+            // 
+            this.button3.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button3.Location = new System.Drawing.Point(328, 242);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 40);
+            this.button3.TabIndex = 23;
+            this.button3.Text = "Cancel";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button4.Location = new System.Drawing.Point(173, 242);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 40);
+            this.button4.TabIndex = 22;
+            this.button4.Text = "Submit";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Form6
             // 
+            this.AcceptButton = this.button4;
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1276, 836);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.CancelButton = this.button3;
+            this.ClientSize = new System.Drawing.Size(608, 344);
+            this.Controls.Add(this.lname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fname);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form6";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Checking Out";
             this.TopMost = true;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox lname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox fname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
