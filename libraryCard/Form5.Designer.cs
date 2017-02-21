cfa81265c7c47f2be0691d07a703c5a727a56605
Kobe Raypole <KDRaypole01@indianatech.net>
2017-02-21 10:45:11 -0500

    Fixed check-in INDEX

--- libraryCard/Form5.Designer.cs
+++ libraryCard/Form5.Designer.cs
@@ -43,58 +43,56 @@
             // button2
             // 
             this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button2.Location = new System.Drawing.Point(282, 289);
+            this.button2.Location = new System.Drawing.Point(205, 193);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(104, 40);
-            this.button2.TabIndex = 9;
+            this.button2.Size = new System.Drawing.Size(76, 27);
+            this.button2.TabIndex = 27;
             this.button2.Text = "Cancel";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(127, 289);
+            this.button1.Location = new System.Drawing.Point(92, 193);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(115, 40);
-            this.button1.TabIndex = 8;
+            this.button1.Size = new System.Drawing.Size(84, 27);
+            this.button1.TabIndex = 26;
             this.button1.Text = "Submit";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // fname
             // 
-            this.fname.Location = new System.Drawing.Point(194, 49);
-            this.fname.Margin = new System.Windows.Forms.Padding(4);
+            this.fname.Location = new System.Drawing.Point(141, 33);
             this.fname.Name = "fname";
-            this.fname.Size = new System.Drawing.Size(287, 29);
+            this.fname.Size = new System.Drawing.Size(210, 22);
             this.fname.TabIndex = 18;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(53, 49);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(39, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(132, 25);
+            this.label1.Size = new System.Drawing.Size(92, 17);
             this.label1.TabIndex = 20;
             this.label1.Text = "Check-Out ID";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(68, 120);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(49, 80);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(95, 25);
+            this.label5.Size = new System.Drawing.Size(67, 17);
             this.label5.TabIndex = 26;
             this.label5.Text = "Condition";
             // 
             // condNew
             // 
             this.condNew.AutoSize = true;
-            this.condNew.Location = new System.Drawing.Point(194, 116);
-            this.condNew.Margin = new System.Windows.Forms.Padding(4);
+            this.condNew.Location = new System.Drawing.Point(141, 77);
             this.condNew.Name = "condNew";
-            this.condNew.Size = new System.Drawing.Size(76, 29);
+            this.condNew.Size = new System.Drawing.Size(56, 21);
             this.condNew.TabIndex = 22;
             this.condNew.TabStop = true;
             this.condNew.Text = "New";
@@ -103,10 +101,9 @@
             // condGood
             // 
             this.condGood.AutoSize = true;
-            this.condGood.Location = new System.Drawing.Point(194, 151);
-            this.condGood.Margin = new System.Windows.Forms.Padding(4);
+            this.condGood.Location = new System.Drawing.Point(141, 101);
             this.condGood.Name = "condGood";
-            this.condGood.Size = new System.Drawing.Size(85, 29);
+            this.condGood.Size = new System.Drawing.Size(64, 21);
             this.condGood.TabIndex = 23;
             this.condGood.TabStop = true;
             this.condGood.Text = "Good";
@@ -115,10 +112,9 @@
             // condFair
             // 
             this.condFair.AutoSize = true;
-            this.condFair.Location = new System.Drawing.Point(194, 184);
-            this.condFair.Margin = new System.Windows.Forms.Padding(4);
+            this.condFair.Location = new System.Drawing.Point(141, 123);
             this.condFair.Name = "condFair";
-            this.condFair.Size = new System.Drawing.Size(70, 29);
+            this.condFair.Size = new System.Drawing.Size(53, 21);
             this.condFair.TabIndex = 24;
             this.condFair.TabStop = true;
             this.condFair.Text = "Fair";
@@ -127,10 +123,9 @@
             // condPoor
             // 
             this.condPoor.AutoSize = true;
-            this.condPoor.Location = new System.Drawing.Point(194, 221);
-            this.condPoor.Margin = new System.Windows.Forms.Padding(4);
+            this.condPoor.Location = new System.Drawing.Point(141, 147);
             this.condPoor.Name = "condPoor";
-            this.condPoor.Size = new System.Drawing.Size(78, 29);
+            this.condPoor.Size = new System.Drawing.Size(59, 21);
             this.condPoor.TabIndex = 25;
             this.condPoor.TabStop = true;
             this.condPoor.Text = "Poor";
@@ -139,11 +134,11 @@
             // Form5
             // 
             this.AcceptButton = this.button1;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.CancelButton = this.button2;
-            this.ClientSize = new System.Drawing.Size(545, 373);
+            this.ClientSize = new System.Drawing.Size(396, 249);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.condNew);
             this.Controls.Add(this.condGood);
@@ -154,6 +149,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "Form5";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
