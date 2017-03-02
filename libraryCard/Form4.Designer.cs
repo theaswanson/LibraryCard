ac2f9dafc26e70e2eec0d1c28fbd177dd9d84f57
Adam Swanson <stapeowner@gmail.com>
2017-03-02 10:11:03 -0500

    Edit customer

--- libraryCard/Form4.Designer.cs
+++ libraryCard/Form4.Designer.cs
@@ -31,20 +31,20 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form4));
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.Id = new System.Windows.Forms.TextBox();
+            this.id = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.fNameLabel = new System.Windows.Forms.Label();
-            this.fName = new System.Windows.Forms.TextBox();
+            this.editField = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(223, 643);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Location = new System.Drawing.Point(122, 145);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(105, 41);
+            this.button2.Size = new System.Drawing.Size(57, 22);
             this.button2.TabIndex = 11;
             this.button2.Text = "Cancel";
             this.button2.UseVisualStyleBackColor = true;
@@ -52,39 +52,40 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(82, 643);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(45, 145);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(105, 41);
+            this.button1.Size = new System.Drawing.Size(57, 22);
             this.button1.TabIndex = 10;
             this.button1.Text = "Edit";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Id
+            // id
             // 
-            this.Id.Location = new System.Drawing.Point(150, 45);
-            this.Id.Margin = new System.Windows.Forms.Padding(4);
-            this.Id.Name = "Id";
-            this.Id.Size = new System.Drawing.Size(202, 29);
-            this.Id.TabIndex = 16;
+            this.id.Location = new System.Drawing.Point(80, 24);
+            this.id.Margin = new System.Windows.Forms.Padding(2);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(112, 20);
+            this.id.TabIndex = 16;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(53, 48);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(34, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 25);
+            this.label1.Size = new System.Drawing.Size(43, 13);
             this.label1.TabIndex = 17;
             this.label1.Text = "User ID";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(53, 116);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(36, 51);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(124, 25);
+            this.label2.Size = new System.Drawing.Size(69, 13);
             this.label2.TabIndex = 18;
             this.label2.Text = "What to Edit:";
             // 
@@ -95,46 +96,49 @@
             this.comboBox1.Items.AddRange(new object[] {
             "First Name",
             "Last Name",
-            "Phone #",
+            "Phone",
             "Address",
             "Date of Birth"});
-            this.comboBox1.Location = new System.Drawing.Point(210, 113);
+            this.comboBox1.Location = new System.Drawing.Point(113, 48);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(142, 32);
+            this.comboBox1.Size = new System.Drawing.Size(79, 21);
             this.comboBox1.TabIndex = 19;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // fNameLabel
             // 
             this.fNameLabel.AutoSize = true;
-            this.fNameLabel.Location = new System.Drawing.Point(41, 203);
+            this.fNameLabel.Location = new System.Drawing.Point(20, 76);
+            this.fNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.fNameLabel.Name = "fNameLabel";
-            this.fNameLabel.Size = new System.Drawing.Size(106, 25);
+            this.fNameLabel.Size = new System.Drawing.Size(57, 13);
             this.fNameLabel.TabIndex = 20;
             this.fNameLabel.Text = "First Name";
             // 
-            // fName
+            // editField
             // 
-            this.fName.Location = new System.Drawing.Point(167, 200);
-            this.fName.Margin = new System.Windows.Forms.Padding(4);
-            this.fName.Name = "fName";
-            this.fName.Size = new System.Drawing.Size(202, 29);
-            this.fName.TabIndex = 21;
+            this.editField.Location = new System.Drawing.Point(80, 73);
+            this.editField.Margin = new System.Windows.Forms.Padding(2);
+            this.editField.Name = "editField";
+            this.editField.Size = new System.Drawing.Size(112, 20);
+            this.editField.TabIndex = 21;
             // 
             // Form4
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(413, 732);
-            this.Controls.Add(this.fName);
+            this.ClientSize = new System.Drawing.Size(225, 188);
+            this.Controls.Add(this.editField);
             this.Controls.Add(this.fNameLabel);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.Id);
+            this.Controls.Add(this.id);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form4";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Edit Customer";
@@ -149,11 +153,11 @@
 
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox Id;
+        private System.Windows.Forms.TextBox id;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label fNameLabel;
-        private System.Windows.Forms.TextBox fName;
+        private System.Windows.Forms.TextBox editField;
     }
 }
--- libraryCard/Form4.cs
+++ libraryCard/Form4.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace libraryCard
 {
     public partial class Form4 : Form
     {
+        string field_, edit_;
+
         public Form4()
         {
             InitializeComponent();
@@ -27,15 +30,61 @@ namespace libraryCard
             if (comboBox1.SelectedIndex == 0)
             {
                 fNameLabel.Show();
-                fName.Show();
+                editField.Show();
             }
         }
 
         private void Form4_Load(object sender, EventArgs e)
         {
             fNameLabel.Hide();
-            fName.Hide();
+            editField.Hide();
            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (this.comboBox1.Text) //converts English table options to database names
+            {
+                case "First Name":
+                    field_ = "FName";
+                    break;
+                case "Last Name":
+                    field_ = "LName";
+                    break;
+                case "Phone":
+                    field_ = "phone";
+                    break;
+                case "Address":
+                    field_ = "address";
+                    break;
+                case "Date of Birth":
+                    field_ = "birthdate";
+                    break;
+                case "":
+                    field_ = "errorInFieldSwitch";
+                    break;
+            }
+
+            string constring = "datasource=" + db_type.db_hostname + ";port=" + db_type.db_port + ";username=" + db_type.db_username + ";password=" + db_type.db_pw;
+            string Query = "USE librarycard UPDATE customers SET " + field_ + "=\"" + this.editField.Text + "\" WHERE customerID=" + this.id.Text + ";";
+            //              UPDATE customers SET     FName     ='     text                   '  WHERE customerID=    1;
+            MySqlConnection conDataBase = new MySqlConnection(constring);
+            MySqlCommand cmdDataBase = new MySqlCommand(Query, conDataBase);
+            MySqlDataReader myReader;
+
+            MessageBox.Show("After query");
+
+            try
+            {
+                conDataBase.Open();
+                myReader = cmdDataBase.ExecuteReader();
+                MessageBox.Show("Customer edited ");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
