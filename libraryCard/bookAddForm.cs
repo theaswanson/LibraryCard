f4d0cf31ced0f3bca8278411ca3d30ee6ae418d2
Serghei Mineev <SMineev01@indianatech.net>
2017-03-23 17:55:59 -0400

    Can add DVDs now

--- libraryCard/bookAddForm.cs
+++ libraryCard/bookAddForm.cs
@@ -41,15 +41,6 @@ namespace libraryCard
 
         private void getData_Click(object sender, EventArgs e)
         {
-            //Console.WriteLine(bookTitle);0
-            /*MessageBox.Show("Title: " + this.bookTitle.Text +
-                "\nAuthor: " + this.author.Text +
-                "\nISBN: " + this.ISBN.Text +
-                "\nGenre: " + this.genre.Text +
-                "\nYear Published: " + this.year.Text +
-                "\nPage Count: " + this.pageCount.Text);*/
-
-
             //checks to see condition of book from 4 bool options
             string cond_ = "";
 
@@ -68,30 +59,62 @@ namespace libraryCard
             else
                 cond_ = "";
 
-
             string constring = "datasource=" + db_type.db_hostname + ";port=" + db_type.db_port + ";username=" + db_type.db_username + ";password=" + db_type.db_pw;
-     
-            string Query = "INSERT INTO librarycard.books (title,author,ISBN,genre,pageCount,bookCondition) values('" + this.bookTitle.Text + "','" + this.author.Text + "','" + this.ISBN.Text + "','" + this.genre.Text + "','" + this.pageCount.Text + "','" + cond_ + "') ;";
-            MySqlConnection conDataBase = new MySqlConnection(constring);
-            MySqlCommand cmdDataBase = new MySqlCommand(Query, conDataBase);
-            MySqlDataReader myReader;
-            try
-            {
-                conDataBase.Open();
-                myReader = cmdDataBase.ExecuteReader();
-                MessageBox.Show("Book Saved: \n" + "Title: " + this.bookTitle.Text +
+
+            //Console.WriteLine(bookTitle);0
+            /*MessageBox.Show("Title: " + this.bookTitle.Text +
                 "\nAuthor: " + this.author.Text +
                 "\nISBN: " + this.ISBN.Text +
                 "\nGenre: " + this.genre.Text +
-                "\nPage Count: " + this.pageCount.Text);
-            //    while (myReader.Read())
-            //    {                         pointless code
-            //    }
+                "\nYear Published: " + this.year.Text +
+                "\nPage Count: " + this.pageCount.Text);*/
+
+            if (comboBookVsDvd.SelectedIndex == 0) //adding a book
+            {
+                string Query = "INSERT INTO librarycard.books (title,author,ISBN,genre,pageCount,bookCondition) values('" + this.bookTitle.Text + "','" + this.author.Text + "','" + this.ISBN.Text + "','" + this.genre.Text + "','" + this.pageCount.Text + "','" + cond_ + "') ;";
+                MySqlConnection conDataBase = new MySqlConnection(constring);
+                MySqlCommand cmdDataBase = new MySqlCommand(Query, conDataBase);
+                MySqlDataReader myReader;
+                try
+                {
+                    conDataBase.Open();
+                    myReader = cmdDataBase.ExecuteReader();
+                    MessageBox.Show("Book Saved: \n" + "Title: " + this.bookTitle.Text +
+                    "\nAuthor: " + this.author.Text +
+                    "\nISBN: " + this.ISBN.Text +
+                    "\nGenre: " + this.genre.Text +
+                    "\nPage Count: " + this.pageCount.Text);
+                    //    while (myReader.Read())
+                    //    {                         pointless code
+                    //    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
             }
-            catch (Exception ex) {
-                MessageBox.Show(ex.Message);
+            else if (comboBookVsDvd.SelectedIndex == 1) //adding a dvd
+            {
+                string Query = "INSERT INTO librarycard.dvd (title,genre,year,dvdCondition) values('" + this.dvdTitle.Text + "','" + this.dvdGenre.Text + "','" + this.dvdYear.Text + "','" + cond_ + "') ;";
+                MySqlConnection conDataBase = new MySqlConnection(constring);
+                MySqlCommand cmdDataBase = new MySqlCommand(Query, conDataBase);
+                MySqlDataReader myReader;
+                try
+                {
+                    conDataBase.Open();
+                    myReader = cmdDataBase.ExecuteReader();
+                    MessageBox.Show("DVD Saved: \n" + "Title: " + this.dvdTitle.Text +
+                    "\nGenre: " + this.dvdGenre.Text +
+                    "\nYear: " + this.dvdYear.Text);
+                    //    while (myReader.Read())
+                    //    {                         pointless code
+                    //    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
             }
-                
         }
 
         private void condFair_CheckedChanged(object sender, EventArgs e)
@@ -146,6 +169,11 @@ namespace libraryCard
 
         private void bookAddForm_Load(object sender, EventArgs e)
         {
+            foreach (Control gPanels in Controls.OfType<Panel>())
+                gPanels.Hide();
+
+            this.comboBookVsDvd.SelectedIndex = 0;
+
             toolTip1.AutoPopDelay = 20000;
         }
 
@@ -172,6 +200,17 @@ namespace libraryCard
         private void label5_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void comboBookVsDvd_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (Control gPanels in Controls.OfType<Panel>())
+                gPanels.Hide();
+
+            if (comboBookVsDvd.SelectedIndex == 0)
+                addBookPanel.Show();
+            else if (comboBookVsDvd.SelectedIndex == 1)
+                addDvdPanel.Show();
         }
     }
 }
--- libraryCard/checkOutForm.cs
+++ libraryCard/checkOutForm.cs
@@ -26,7 +26,11 @@ namespace libraryCard
 
         private void button4_Click(object sender, EventArgs e)
         {
-            if (comboBoxID_ISBN.Text == "Book using ID")
+            if (comboBoxID_ISBN.SelectedIndex == 0) //Book using ISBN
+            {
+
+            }
+            else if (comboBoxID_ISBN.SelectedIndex == 1) //Book using ID
             {
                 string outDate = DateTime.Now.ToString("yyyyMMddHHmmss");
                 string constring = "datasource=" + db_type.db_hostname + ";port=" + db_type.db_port + ";username=" + db_type.db_username + ";password=" + db_type.db_pw;
@@ -46,11 +50,7 @@ namespace libraryCard
                     MessageBox.Show(ex.Message);
                 }
             }
-            else if (comboBoxID_ISBN.Text == "Book using ISBN")
-            {
-
-            }
-            else if (comboBoxID_ISBN.Text == "DVD using ID")
+            else if (comboBoxID_ISBN.SelectedIndex == 2) //DVD using ID
             {
 
             }
@@ -58,6 +58,9 @@ namespace libraryCard
 
         private void Form6_Load(object sender, EventArgs e)
         {
+            foreach (Control gBoxes in Controls.OfType<GroupBox>())
+                gBoxes.Hide();
+
             this.comboBoxID_ISBN.SelectedIndex = 1;
         }
 
@@ -68,7 +71,21 @@ namespace libraryCard
 
         private void comboBoxID_ISBN_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            foreach (Control gBoxes in Controls.OfType<GroupBox>())
+                gBoxes.Hide();
+
+            if (comboBoxID_ISBN.SelectedIndex == 0)
+                bookIsbnBox.Show();
+            else if (comboBoxID_ISBN.SelectedIndex == 1)
+                bookIdBox.Show();
+            else if (comboBoxID_ISBN.SelectedIndex == 2)
+                dvdIdBox.Show();
+
+        }
+
+        private void bookIdBox_Enter(object sender, EventArgs e)
+        {
+
         }
     }
 }
